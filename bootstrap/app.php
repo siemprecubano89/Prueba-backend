a2dfd8b0664a2df972dc22d14a7c9aec1bdc053b
Ricardo Rodríguez García <44219469+siemprecubano89@users.noreply.github.com>
2025-12-10 12:23:03 -0600

    ✅ - Crear los tests necesarios para validar los cambios realizados. ✅ - Asegurarse de que todos los tests pasen después de realizar los cambios.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,9 +7,15 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
+        api: __DIR__ . '/../routes/api.php', // Delete, use v1 instead
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        then: function ($router) {
+            // API V1
+            Route::middleware('api')
+                ->prefix('v1')
+                ->group(base_path('routes/api.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
